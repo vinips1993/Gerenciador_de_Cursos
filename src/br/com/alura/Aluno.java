404db47db4348648f3e9a5a4f6b358123449433e
vinips1993 <rm550907@fiap.com.br>
2024-04-26 20:19:41 -0300

    Criando a Classe TestaCursoComAluno

--- src/br/com/alura/Aluno.java
+++ src/br/com/alura/Aluno.java
@@ -4,6 +4,13 @@ public class Aluno {
 
     private String nome;
     private int numeroDaMatricula;
+
+    public Aluno(String nome, int numeroDaMatricula) {
+        this.nome = nome;
+        this.numeroDaMatricula = numeroDaMatricula;
+        this.numeroMatricula = numeroMatricula;
+    }
+
     private String numeroMatricula;
 
     public String getNome() {
